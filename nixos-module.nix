6cd480ebc0464115a7b09a8aac23dd7002e71141
lucasew <lucas59356@gmail.com>
2022-07-15 17:01:41 -0300

    add nix/nixos stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ nixos-module.nix
@@ -0,0 +1,31 @@
+{config, pkgs, lib, ...}:
+let
+  cfg = config.services.simple-dashboardd;
+  module = pkgs.callPackage ./default.nix {};
+in {
+  options = with lib; {
+    services.simple-dashboardd = {
+      enable = mkEnableOption "Webapp to show system usage";
+      config = mkOption {
+        description = "Config string to be used by simple-dashboard";
+        type = types.lines;
+        default = builtins.readFile "${module}/share/simple-dashboard/config.ini.example";
+      };
+      port = mkOption {
+        description = "Port to listen";
+        default = 42069;
+        type = types.port;
+      };
+    };
+  };
+  config = with lib; mkIf cfg.enable {
+    systemd.services.simple-dashboardd = {
+      enable = true;
+      path = [ module ];
+      script = ''
+        ${module}/bin/simple-dashboardd -c ${builtins.toFile "simple-dashboard.cfg" cfg.config} -p ${builtins.toString cfg.port}
+      '';
+      restartIfChanged = true;
+    };
+  };
+}
